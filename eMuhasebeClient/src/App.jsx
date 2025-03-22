3857ef7b45edb9dbab7f220bbb379cf1c6c75f97
Emrullah Yavuz <85703039+emrullahyavuz@users.noreply.github.com>
2025-03-22 17:37:50 +0300

    Add Button component and utility functions for class name management

--- eMuhasebeClient/src/App.jsx
+++ eMuhasebeClient/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Sidebar from "./components/Sidebar"
 import "./App.css"
+import { Button } from "./components/UI/Button"
 
 function App() {
   const [darkMode, setDarkMode] = useState(true)
@@ -19,7 +20,7 @@ function App() {
       <div className="app-container">
         <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
         <div className={`content ${sidebarOpen ? "sidebar-open" : ""}`}>
-          
+          <Button />
         </div>
       </div>
     </div>
